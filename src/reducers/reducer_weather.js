d91b2910de803a077e098a53d59787f140d480ff
Steven Starnes <starnes81@gmail.com>
2017-09-21 20:18:03 -0500

    set up switch
    Set up switch case for default state to retturn array.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,9 @@
-export default function(state = null, action) {
-	console.log('Action Received', action);
+import { FETCH WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+	switch(action.type) {
+		case FETCH_WEATHER:
+			return [action.payload.data, ...state];
+	}
 	return state;
 }
